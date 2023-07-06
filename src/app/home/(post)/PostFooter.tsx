589f58249db3d32333818e0574f375553d0f95e1
CarelessInternet <myinternetisg00d@gmail.com>
2023-07-06 12:55:55 +0200

    feat(like): seamless transition to like and unlike

--- src/app/home/(post)/PostFooter.tsx
+++ src/app/home/(post)/PostFooter.tsx
@@ -2,7 +2,7 @@
 
 import { Button, CardFooter } from '@nextui-org/react';
 import { IconBlockquote, IconHeart, IconHeartFilled, IconMessageCircle } from '@tabler/icons-react';
-import { experimental_useOptimistic as useOptimistic } from 'react';
+import { experimental_useOptimistic as useOptimistic, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { likePost } from '../actions';
 import type { PostData } from '@/utils';
@@ -14,7 +14,9 @@ export default function PostFooter({ post }: { post: PostData }) {
 		(state, newLikeCount: number) => ({ ...state, likeCount: newLikeCount })
 	);
 
-	const hasUserLiked = post.likes.find((like) => like.userId === session?.user.id);
+	const [hasUserLiked, setHasUserLiked] = useState(
+		Boolean(post.likes.find((like) => like.userId === session?.user.id))
+	);
 
 	return (
 		<CardFooter className="flex justify-between">
@@ -34,6 +36,8 @@ export default function PostFooter({ post }: { post: PostData }) {
 					addOptimisticLike(
 						!hasUserLiked ? optimisticLikes.likeCount + 1 : optimisticLikes.likeCount - 1
 					);
+					setHasUserLiked((bool) => !bool);
+
 					await likePost(post);
 				}}
 			>
